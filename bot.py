446a5da8ca008d321f053c922e666dfa32a70b9c
z e r o <131664327+zero-vps@users.noreply.github.com>
2024-03-19 01:58:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -487,7 +487,7 @@ __• Owner can Broadcast Message to Users.__
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("• Channel •", url="https://t.me/Anaavaran")
+                [InlineKeyboardButton("• Channel •", url="https://t.me/Anaavaran"),
                 InlineKeyboardButton("• Owner •", url="https://t.me/itsmeflashh")
                    ],[ 
 	       InlineKeyboardButton("• Close •", callback_data="close")],
