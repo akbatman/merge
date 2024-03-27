b3aa164527ede71463b032add1ee8f94a61e17c4
z e r o <131664327+zero-vps@users.noreply.github.com>
2024-03-27 17:49:18 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -39,7 +39,7 @@ async def uploadVideo(
                     caption=f"<i>{merged_video_path.rsplit('/',1)[-1]}</i>\n\nn🛰️ <b>Merged by:</b> {cb.from_user.mention}",
                     progress=prog.progress_for_pyrogram,
                     progress_args=(
-                        f"<b>Status: 📤Uploading....</b>\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
+                        f"<b>Status: 📤Uploading....</b>\n\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
                         c_time,
                     ),
                 )
@@ -52,7 +52,7 @@ async def uploadVideo(
                     caption=f"<i>{merged_video_path.rsplit('/',1)[-1]}</i>\n\n🛰️ <b>Merged by:</b> <a href='tg://user?id={cb.from_user.id}'>{cb.from_user.first_name}</a>",
                     progress=prog.progress_for_pyrogram,
                     progress_args=(
-                        f"<b>Status: 📤Uploading....</b>\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
+                        f"<b>Status: 📤Uploading....</b>\n\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
                         c_time,
                     ),
                 )
@@ -80,7 +80,7 @@ async def uploadVideo(
                     caption=f"<b>{merged_video_path.rsplit('/',1)[-1]}</b>",
                     progress=prog.progress_for_pyrogram,
                     progress_args=(
-                        f"<b>Status: 📤Uploading....</b>\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
+                        f"<b>Status: 📤Uploading....</b>\n\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
                         c_time,
                     ),
                 )
@@ -93,7 +93,7 @@ async def uploadVideo(
                     caption=f"<b>{merged_video_path.rsplit('/',1)[-1]}</b>",
                     progress=prog.progress_for_pyrogram,
                     progress_args=(
-                        f"<b>Status: 📤Uploading....</b>\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
+                        f"<b>Status: 📤Uploading....</b>\n\n<b>📁 Name: </b><i>{merged_video_path.rsplit('/',1)[-1]}</i>",
                         c_time,
                     ),
                 )
@@ -126,7 +126,7 @@ async def uploadFiles(
             caption=f"<b>{up_path.rsplit('/',1)[-1]}</b>",
             progress=prog.progress_for_pyrogram,
             progress_args=(
-                f"<b>Status: 📤Uploading....</b>\n<b>📁 Name: </b><i>{up_path.rsplit('/',1)[-1]}<i>",
+                f"<b>Status: 📤Uploading....</b>\n\n<b>📁 Name: </b><i>{up_path.rsplit('/',1)[-1]}<i>",
                 c_time,
                 f"\n**Uploading: {n}/{all}**"
             ),
