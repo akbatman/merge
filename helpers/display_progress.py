e603c62f0ae3d725622e32c4f9012ed697a69eed
z e r o <131664327+zero-vps@users.noreply.github.com>
2024-03-27 22:28:50 +0530

    Update display_progress.py

--- helpers/display_progress.py
+++ helpers/display_progress.py
@@ -78,12 +78,12 @@ class Progress:
 
             progress = "{0}{1} <b>{2}%</b>\n".format(
                 "".join(
-                    FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))
+                    FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))
                 ),
                 "".join(
                     
                         UN_FINISHED_PROGRESS_STR
-                        for i in range(20 - math.floor(percentage / 5))
+                        for i in range(10 - math.floor(percentage / 10))
                     
                 ),
                 round(percentage, 2),
