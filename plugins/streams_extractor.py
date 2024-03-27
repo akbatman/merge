e378a874dd2d48968d1d87571df67af5adf67f55
z e r o <131664327+zero-vps@users.noreply.github.com>
2024-03-27 17:43:36 +0530

    Update streams_extractor.py

--- plugins/streams_extractor.py
+++ plugins/streams_extractor.py
@@ -28,7 +28,7 @@ async def streamsExtractor(c: Client, cb:CallbackQuery ,media_mid, exAudios=Fals
     try:
         c_time = time.time()
         prog = Progress(cb.from_user.id, c, cb.message)
-        progress=f"<b>📁 Name:</b> <i>{media.file_name}</i>\n\n<b>Status:</b> 🚀Downloading..."
+        progress=f"<b>Status: 🚀Downloading...</b>\n\n<b>📁 Name:</b> <i>{media.file_name}</i>"
         file_dl_path = await c.download_media(
             message=media,
             file_name=f"downloads/{str(cb.from_user.id)}/{str(omess.id)}/vid.mkv",  # fix for filename with single quote(') in name
