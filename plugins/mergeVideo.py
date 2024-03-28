50d0916bc511ae9a75dc48f466d23489ac52bd45
z e r o <131664327+zero-vps@users.noreply.github.com>
2024-03-28 16:26:03 +0530

    Update mergeVideo.py

--- plugins/mergeVideo.py
+++ plugins/mergeVideo.py
@@ -58,7 +58,7 @@ chat_id=cb.from_user.id, message_ids=list_message_ids ):
                 message=media,
                 file_name=f"downloads/{str(cb.from_user.id)}/{str(i.id)}/vid.mkv",  # fix for filename with single quote(') in name
                 progress=prog.progress_for_pyrogram,
-                progress_args=(f"<b>〘 📤 Downloading File - {n}/{all} 〙</b>\n\n<b>📁 Name:</b> <i>{media.file_name}</i>", c_time),
+                progress_args=(f"<b>〘 📥 Downloading File - {n}/{all} 〙</b>\n\n<b>📁 Name:</b> <i>{media.file_name}</i>", c_time),
             )
             n+=1
             if gDict[cb.message.chat.id] and cb.message.id in gDict[cb.message.chat.id]:
