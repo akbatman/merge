30869b6d5412942a4676e7e66388f16cc2c86aa6
z e r o <131664327+zero-vps@users.noreply.github.com>
2024-03-30 12:30:21 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -52,11 +52,70 @@ async def uploadVideo(
                 )
                 # ... rest of your code within the if block ...
             else:
-                # ... similar logic for upload_mode True ...
-
+                  c_time = time.time()
+                sent_: Message = await userBot.send_document(
+                    chat_id=int(LOGCHANNEL),
+                    document=merged_video_path,
+                    thumb=video_thumbnail,
+                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`\n\nMerged by: <a href='tg://user?id={cb.from_user.id}'>{cb.from_user.first_name}</a>",
+                    progress=prog.progress_for_pyrogram,
+                    progress_args=(
+                        f"Uploading: `{merged_video_path.rsplit('/',1)[-1]}`",
+                        c_time,
+                    ),
+                )
+            if sent_ is not None:
+                await c.copy_message(
+                    chat_id=cb.message.chat.id,
+                    from_chat_id=sent_.chat.id,
+                    message_id=sent_.id,
+                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`",
+                )
+                # await sent_.delete()
     else:
-        # ... rest of your code for non-premium users ...
-
+        try:
+            sent_ = None
+            prog = Progress(cb.from_user.id, c, cb.message)
+            if upload_mode is False:
+                c_time = time.time()
+                sent_: Message = await c.send_video(
+                    chat_id=cb.message.chat.id,
+                    video=merged_video_path,
+                    height=height,
+                    width=width,
+                    duration=duration,
+                    thumb=video_thumbnail,
+                    caption=f"<b>{merged_video_path.rsplit('/',1)[-1]}</b>",
+                    progress=prog.progress_for_pyrogram,
+                    progress_args=(
+                        f"Uploading: `{merged_video_path.rsplit('/',1)[-1]}`",
+                        c_time,
+                    ),
+                )
+            else:
+                c_time = time.time()
+                sent_: Message = await c.send_document(
+                    chat_id=cb.message.chat.id,
+                    document=merged_video_path,
+                    thumb=video_thumbnail,
+                    caption=f"`{merged_video_path.rsplit('/',1)[-1]}`",
+                    progress=prog.progress_for_pyrogram,
+                    progress_args=(
+                        f"Uploading: `{merged_video_path.rsplit('/',1)[-1]}`",
+                        c_time,
+                    ),
+                )
+        except Exception as err:
+            LOGGER.info(err)
+            await cb.message.edit("Failed to upload")
+        if sent_ is not None:
+            if Config.LOGCHANNEL is not None:
+                media = sent_.video or sent_.document
+                await sent_.copy(
+                    chat_id=int(LOGCHANNEL),
+                    caption=f"`{media.file_name}`\n\nMerged by: <a href='tg://user?id={cb.from_user.id}'>{cb.from_user.first_name}</a>",
+                )
+                
 
 async def uploadFiles(
     c: Client,
@@ -88,6 +147,3 @@ async def uploadFiles(
                 )
     except:
         pass
-
-
-# ... rest of your code ...
